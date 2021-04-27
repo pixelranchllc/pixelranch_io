986a1a808532906f0585b583f3cce345b950fcda
dtolsen <dtolsen@protonmail.com>
2021-04-26 22:10:46 -0600

    Troubleshooting form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,6 +14,7 @@ const Contact = () => (
         name="form-pixelranch-contact"
         method="POST"
         data-netlify="true"
+        netlify
       >
         <fieldset>
           <label htmlFor="name">
